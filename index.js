972224e1b544699d519f8b008d02d30067668b6c
Kathyclm <clm1978@gmail.com>
2022-10-30 10:34:57 +1000

    add tutorial assignment file

---
+++ index.js
@@ -0,0 +1,9 @@
+let = 'hello'
+console.log(let);
+
+const weight = prompt ('please enter your weight in kg');
+const height = prompt ('please enter your weight in meter');
+
+const BMI = weight / (height * height);
+
+console.log(`Your BMI is ${BMI}`);
